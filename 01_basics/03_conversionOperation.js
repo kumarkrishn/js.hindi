4ccf69412c11a3723b9f8f9b60dcffc74f040638
Krishn Kumar <86828028+kumarkrishn@users.noreply.github.com>
2025-06-14 10:48:44 +0000

    Conveersion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = null
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "Hitesh" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 0
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Radhe Radhe" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(someNumber)
+ console.log(typeof stringNumber)
