57a70efa9dc5fe60342fda02bbd36c23a1941e04
Krishn Kumar <86828028+kumarkrishn@users.noreply.github.com>
2025-06-14 10:51:02 +0000

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version of JS
+//alert(3+3) // it will raise error because we are using node js not browser
+console.log(3 
+    +3 ) // code redability should be high
+console.log("Radhe Radhe") 
+
+//window.alert("Jai Shree Radhe!!!!!")
+
+let name = "Radha Rani"
+let age  = 26
+let isLoggedIn = false
+
+//DATA TYPES IS GIVEN BELOW
+//number => 2 to the power of 53
+//bingint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+
+    console.log(typeof age)
+    console.log(typeof null)
+    console.log(typeof undefined)
